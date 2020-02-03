e6d85d6c69be7fd4febf7ed3e114a03343edffd4
Peter Reimer <reimer@hbz-nrw.de>
2020-02-03 16:55:53 +0100

    add usage function

--- check_pywb_indexsize.sh
+++ check_pywb_indexsize.sh
@@ -6,25 +6,24 @@
 # und schickt eine Mail, sobald der Indexe die kritische Größe erreicht.
 # Das Skript erwartet die Angabe der maximalen Größe in MB.
 #
-# Aufruf:
-# $ check_pywb_indexsize.sh <pywb-index> <maximale Größe>
 # Aufruf in monit Konfiguration mit absoluten Pfaden
-# $ /opt/pywb/bin/check_pywb_indexsize.sh /opt/pywb/collections/weltweit/indexes/index.cdxj 10000
+# $ /opt/pywb/bin/check_pywb_indexsize.sh -i /opt/pywb/collections/weltweit/indexes/index.cdxj -m 10000 -l weltweit.log
 
+usage() { echo "Usage: $0 [-i <pywb index>] [-m <max size in MB>] [-l <logfile>]" 1>&2; exit 1; }
 
-while getopts i:m:l: option; do
+while getopts ":i:m:l:" option; do
     case "${option}" in
-        i) INDEX=${OPTARG};;
-        m) MAXSIZE=${OPTARG};;
-        l) LOG=${OPTARG};;
+        i ) INDEX=${OPTARG};;
+        m ) MAXSIZE=${OPTARG};;
+        l ) LOG=${OPTARG};;
+	\?) usage;;
     esac
 done
 
+shift $(($OPTIND -1))
 
 LOGDIR="/opt/pywb/logs"
 logfile=$LOGDIR/$LOG
-
-echo $logfile
 
 size=$(du -m "$INDEX" | cut -f 1)
 time=`date +%F\ %T`
